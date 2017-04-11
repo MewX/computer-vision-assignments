49535597536a21ac0ec5ce4482eba06660c66e34
MewX <imewx@qq.com>
2017-04-11 21:01:49 +0930

    Changed remote execution strategy due to a Windows issue

--- reports/report1/drag.sh
+++ reports/report1/drag.sh
@@ -2,7 +2,7 @@
 scp main.tex a1700831@uss.cs.adelaide.edu.au:/users/1/a1700831/Codes/cv-reports/
 
 # compile first
-ssh a1700831@uss.cs.adelaide.edu.au 'sh -s' < remote.sh
+ssh a1700831@uss.cs.adelaide.edu.au "cd /users/1/a1700831/Codes/cv-reports && sh -x remote.sh"
 
 # drag back
 scp a1700831@uss.cs.adelaide.edu.au:/users/1/a1700831/Codes/cv-reports/main.pdf main.pdf
--- reports/report1/remote.sh
+++ reports/report1/remote.sh
@@ -1,4 +1,3 @@
-cd /users/1/a1700831/Codes/cv-reports
 ~/Downloads/tex/texlive/2016/bin/x86_64-linux/pdflatex main &>null
 ~/Downloads/tex/texlive/2016/bin/x86_64-linux/bibtex main
 ~/Downloads/tex/texlive/2016/bin/x86_64-linux/pdflatex main
