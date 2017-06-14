3559fe581c1997931fbb3df7332c6aded8ffcc82
MewX <imewx@qq.com>
2017-06-14 11:33:52 +0930

    Added init codes

---
+++ assignment3/extract_features.sh
@@ -0,0 +1,7 @@
+# csh
+# $1: image directory, containing tasks.txt (all image names)
+
+# preq:
+# feature model definition: python_features/deploy_features.prototxt
+# feature model caffe net: checkpoints/VGG_ILSVRC_16_layers.caffemodel
+python py_caffe_feat_extract.py --model_def_path python_features/deploy_features.prototxt --model_path checkpoints/VGG_ILSVRC_16_layers.caffemodel -i "$1/" --filter "$1/tasks.txt" -o "$1/"
---
+++ assignment3/parse_data_set.py
@@ -0,0 +1,10 @@
+import json
+
+# take input: [img dir] [train img list] [sentences file]
+'''
+JSON format:
+{
+    images: [],
+
+}
+'''
