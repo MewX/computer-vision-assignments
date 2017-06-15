e258dc4fe99851904d14ca1e3a4a50e36bc51d9e
MewX <imewx@qq.com>
2017-06-15 21:27:17 +0930

    Cleaned working space

--- assignment3/datasets/dl_all.sh
+++ assignment3/datasets/dl_all.sh
@@ -2,3 +2,7 @@ wget -c http://nlp.cs.illinois.edu/HockenmaierGroup/Framing_Image_Description/Fl
 unzip Flickr8k_Dataset.zip
 wget -c http://nlp.cs.illinois.edu/HockenmaierGroup/Framing_Image_Description/Flickr8k_text.zip
 unzip Flickr8k_text.zip
+
+# the token only
+wget -c http://shannon.cs.illinois.edu/DenotationGraph/data/flickr30k.tar.gz
+tar zxvf flickr30k.tar.gz
