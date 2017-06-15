4142448f8cae6783f609c493942394188032ec88
MewX <imewx@qq.com>
2017-06-15 20:06:16 +0930

    Cleaned codes

--- assignment3/benchmark_solution.py
+++ assignment3/benchmark_solution.py
@@ -56,36 +56,49 @@ A = ['123', '12', '1', '12']
 B = ['12', '1', '12', '123']
 
 
-def lcs(S, T):
-    m = len(S)
-    n = len(T)
-    counter = [[0]*(n+1) for x in range(m+1)]
-    longest = 0
-    lcs_list = list()
-    for i in range(m):
-        for j in range(n):
-            if S[i] == T[j]:
-                c = counter[i][j] + 1
-                counter[i+1][j+1] = c
-                if c > longest:
-                    lcs_list = list()
-                    longest = c
-                    lcs_list.append(S[i-c+1:i+1])
-                elif c == longest:
-                    lcs_list.append(S[i-c+1:i+1])
+def lcs(a, b):
+    print(a)
+    print(b)
+    lengths = [[0 for j in range(len(b)+1)] for i in range(len(a)+1)]
+    # row 0 and column 0 are initialized to 0 already
+    for i, x in enumerate(a):
+        for j, y in enumerate(b):
+            if x == y:
+                lengths[i+1][j+1] = lengths[i][j] + 1
+            else:
+                lengths[i+1][j+1] = max(lengths[i+1][j], lengths[i][j+1])
+    # read the substring out from the matrix
+    result = []
+    x, y = len(a), len(b)
+    while x != 0 and y != 0:
+        if lengths[x][y] == lengths[x-1][y]:
+            x -= 1
+        elif lengths[x][y] == lengths[x][y-1]:
+            y -= 1
+        else:
+            assert a[x-1] == b[y-1]
+            result.insert(0, a[x-1])
+            x -= 1
+            y -= 1
+    print(result)
+    return len(result)
 
-    return len(lcs_list)
 
-
-def benchmark(a, b):
+def benchmark_s(a, b):
     set_a = set(a)
     set_b = set(b)
     sameWords = set.intersection(set_a, set_b)
     p_s = len(sameWords) / ((len(set_a) + len(set_b)) / 2)
+    return p_s
 
-    # p_lcs = lcs(a, b) / ((len(a) + len(b)) / 2)
-    p_lcs = 1
-    return p_s * p_lcs
+
+def benchmark_lcs(a, b):
+    p_lcs = lcs(a, b) / ((len(a) + len(b)) / 2)
+    return p_lcs
+
+
+def benchmark_all(s, lcs):
+    return (s + lcs) / 2
 
 
 DATA = int(sys.argv[1])  # 8k or 30k
@@ -98,7 +111,6 @@ tokens = [x for x in content]
 regex = r"(.+?\.jpg.*?)#(\d+)\t(.+?)$"
 token_map = {}
 for line in tokens:
-    # print("{} - {}\n".format(regex, line))
     obj = re.match(regex, line)
     name = obj.group(1)
     sentence = obj.group(3).lower().replace(".", "").strip()
@@ -112,26 +124,40 @@ for line in tokens:
 #     print(x)
 #     print(token_map[x])
 
+
 # read the result_struct.json file
 # argv[2]: the result_struct.json file name
 print("Loading {}\n".format(sys.argv[2]))
 with open(sys.argv[2]) as data_file:
     parsed_json = json.load(data_file)
 img_blobs = parsed_json['imgblobs']
+max_bm = 0.0
+min_bm = 1.0
+avg_bm = 0.0
 for img in img_blobs:
     file_name = img['img_path']
     sentence = img['candidate']['text']
 
     # calc result
     m = 0.0
+    p_s = 0.0
+    p_lcs = 0.0
     for x in token_map[file_name]:
-        temp = benchmark(sentence, x)
-        m = max(temp, m)
-    print("{}: {}".format(file_name, "%.4f" % m))
-    # print("{}: {}\n".format(file_name, sentence))
+        # prepare
+        a = re.compile(r"\s+").split(sentence)
+        b = re.compile(r"\s+").split(x)
 
+        temp_s = benchmark_s(a, b)
+        temp_lcs = benchmark_lcs(a, b)
+        temp = benchmark_all(temp_s, temp_lcs)
+        if temp > m:
+            m = temp
+            p_s = temp_s
+            p_lcs = temp_lcs
+    max_bm = max(max_bm, m)
+    min_bm = min(min_bm, m)
+    avg_bm += m
+    print("{}: {} & {} -> {}".format(file_name, "%.4f" % p_s, "%.4f" % p_lcs, "%.4f" % m))
+avg_bm /= len(img_blobs)
+print("max: {}, min: {}, avg: {}".format(max_bm,min_bm,avg_bm))
 
-# parse prediction results
-# build image ~ text table
-# fake longest common substring
-# average benchmark: percentage of matched words * LCS/avg length
--- assignment3/parse_data_set.py
+++ assignment3/parse_data_set.py
@@ -1,6 +1,9 @@
 import json
 
 # take input: [img dir] [train img list] [sentences file]
+import re
+import sys
+
 '''
 JSON format:
 {
@@ -16,3 +19,18 @@ JSON format:
 
 
 regex = "^(.*?\.jpg)#(\d+)\t(.*?)$"
+with open(sys.argv[1]) as f:
+    content = f.readlines()
+tokens = [x for x in content]
+
+token_map = {}
+for line in tokens:
+    obj = re.match(regex, line)
+    name = obj.group(1)
+    sentence = obj.group(3).lower().replace(".", "").strip()
+    if name in token_map:
+        token_map[name].append(sentence)
+    else:
+        token_map[name] = [sentence]
+
+# TODO: this code is not finished
